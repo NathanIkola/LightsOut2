de4b1d12dc56044c933381bd83a4ccd5f540634d
NathanIkola <52811935+NathanIkola@users.noreply.github.com>
2024-01-18 16:19:42 -0600

    Add support for autodoors
    Add support for putting autodoors in standby when not in use.

---
+++ Source/LightsOut2/LightsOut2/Patches/Building_Door_DoorOpen.cs
@@ -0,0 +1,29 @@
+﻿using HarmonyLib;
+using LightsOut2.Common;
+using LightsOut2.ThingComps;
+using RimWorld;
+
+namespace LightsOut2.Patches
+{
+    /// <summary>
+    /// The class that allows autodoors to enter standby
+    /// </summary>
+    [HarmonyPatch(typeof(Building_Door), "DoorOpen")]
+    public class Building_Door_DoorOpen
+    {
+        /// <summary>
+        /// Postfix patch so that we can detect the moment the door is marked as open
+        /// </summary>
+        /// <param name="__instance"></param>
+        public static void Postfix(Building_Door __instance)
+        {
+            // manual doors are not affected
+            if (!__instance.DoorPowerOn) return;
+
+            StandbyComp comp = __instance.GetStandbyComp();
+            if (comp is null) return;
+
+            comp.IsInStandby = !__instance.Open;
+        }
+    }
+}
---
+++ Source/LightsOut2/LightsOut2/Patches/Building_Door_DoorTryClose.cs
@@ -0,0 +1,30 @@
+﻿using HarmonyLib;
+using LightsOut2.Common;
+using LightsOut2.ThingComps;
+using RimWorld;
+
+namespace LightsOut2.Patches
+{
+    /// <summary>
+    /// The class that handles autodoors closing
+    /// </summary>
+    [HarmonyPatch(nameof(Building_Door), "DoorTryClose")]
+    public class Building_Door_DoorTryClose
+    {
+        /// <summary>
+        /// The patch that actually sets the autodoor back into standby
+        /// </summary>
+        /// <param name="__instance">The door to affect</param>
+        /// <param name="__result">Whether the door closed successfully</param>
+        public static void Postfix(Building_Door __instance, bool __result)
+        {
+            // if the door did not close successfully, ignore this
+            if (!__result) return;
+
+            // otherwise put the door into standby
+            StandbyComp comp = __instance.GetStandbyComp();
+            if (comp is null) return;
+            comp.IsInStandby = true;
+        }
+    }
+}
