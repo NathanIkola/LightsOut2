7183790ecc604c416b4dc62a921485d7a7b196b5
NathanIkola <52811935+NathanIkola@users.noreply.github.com>
2024-05-28 19:51:03 -0500

    Update the post-build script
    Change directory before executing the custom post-build so that it's
    easier to use

--- Source/post-build.cmd
+++ Source/post-build.cmd
@@ -10,4 +10,6 @@ echo f | xcopy /f /Y "LightsOut2.dll" "../../Assemblies/LightsOut2.dll"
 echo f | xcopy /f /Y "LightsOut2.Core.dll" "../../Assemblies/LightsOut2.Core.dll"
 echo f | xcopy /f /Y "LightsOut2.ModCompatibility.dll" "../../Assemblies/LightsOut2.ModCompatibility.dll"
 
-if exist "../post-build-custom.cmd" ( call ../post-build-custom.cmd )
+:: Pop up to the Source directory and execute the custom post-build if it's present
+cd ..
+if exist "post-build-custom.cmd" ( call post-build-custom.cmd )
