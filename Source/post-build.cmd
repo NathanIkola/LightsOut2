cf646210d97f6d2aeb64a3f3d06c6164f1708de4
NathanIkola <52811935+NathanIkola@users.noreply.github.com>
2024-05-28 19:35:04 -0500

    Fix post-build script
    Correct the post-build script to be able to actually target the right
    files at the right time and detect the custom post-build script

--- Source/post-build.cmd
+++ Source/post-build.cmd
@@ -1,12 +1,13 @@
 @echo off
+echo %cd%
 
 :: Delete the existing assemblies
-rmdir "../Assemblies" /S /Q
+rmdir "../../Assemblies" /S /Q
 
 :: Copy the newly-built assemblies
-mkdir "../Assemblies"
-echo f | xcopy /f /Y "Build/LightsOut2.dll" "../Assemblies/LightsOut2.dll"
-echo f | xcopy /f /Y "Build/LightsOut2.Core.dll" "../Assemblies/LightsOut2.Core.dll"
-echo f | xcopy /f /Y "Build/LightsOut2.ModCompatibility.dll" "../Assemblies/LightsOut2.ModCompatibility.dll"
+mkdir "../../Assemblies"
+echo f | xcopy /f /Y "LightsOut2.dll" "../../Assemblies/LightsOut2.dll"
+echo f | xcopy /f /Y "LightsOut2.Core.dll" "../../Assemblies/LightsOut2.Core.dll"
+echo f | xcopy /f /Y "LightsOut2.ModCompatibility.dll" "../../Assemblies/LightsOut2.ModCompatibility.dll"
 
-if exist "post-build-custom.cmd" ( call post-build-custom.cmd )
+if exist "../post-build-custom.cmd" ( call ../post-build-custom.cmd )
