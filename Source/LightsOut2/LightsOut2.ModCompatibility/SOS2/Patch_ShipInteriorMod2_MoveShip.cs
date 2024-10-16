33af81a1fd0fb446662e6729dc0f7ba4372bc94f
NathanIkola <52811935+NathanIkola@users.noreply.github.com>
2024-10-15 22:32:11 -0400

    Add support for SOS2
    Add support for ship moves via SOS2

---
+++ Source/LightsOut2/LightsOut2.ModCompatibility/SOS2/Patch_ShipInteriorMod2_MoveShip.cs
@@ -0,0 +1,49 @@
+﻿using LightsOut2.Common;
+using LightsOut2.Core.ModCompatibility;
+using LightsOut2.Patches;
+using System;
+using System.Collections.Generic;
+using Verse;
+
+namespace LightsOut2.ModCompatibility.SOS2
+{
+    public class Patch_ShipInteriorMod2_MoveShip : IModCompatibilityPatchComponent
+    {
+        public override string TypeNameToPatch => "ShipInteriorMod2";
+
+        public override bool TargetsMultipleTypes => false;
+
+        public override bool TypeNameIsExact => true;
+
+        public override string ComponentName => "Patch MoveShip to turn on lights after moving";
+
+        public override IEnumerable<PatchInfo> GetPatches(Type type)
+        {
+            PatchInfo patch = new PatchInfo
+            {
+                method = GetMethod(type, "MoveShip"),
+                patch = GetMethod<Patch_ShipInteriorMod2_MoveShip>(nameof(Postfix)),
+                patchType = PatchType.Postfix,
+            };
+
+            return new List<PatchInfo>() { patch };
+        }
+
+        public static void Postfix()
+        {
+            Map map = Find.CurrentMap;
+            if (map is null)
+                return;
+
+            // loop over all rooms on the map and update their occupancy
+            foreach(Room room in map.regionGrid.allRooms)
+            {
+                if (room is null) 
+                    continue;
+
+                bool hasOccupants = !Utils.IsRoomEmpty(room, null);
+                Pawn_PathFollower_TryEnterNextPathCell.RaiseOnRoomOccupancyChangedEvent(room, hasOccupants, false);
+            }
+        }
+    }
+}
---
+++ Source/LightsOut2/LightsOut2.ModCompatibility/SOS2/SOS2CompatibilityPatch.cs
@@ -0,0 +1,24 @@
+﻿using LightsOut2.Core.ModCompatibility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LightsOut2.ModCompatibility.SOS2
+{
+    public class SOS2CompatibilityPatch : IModCompatibilityPatch
+    {
+        public override string CompatibilityPatchName => "SOS2";
+
+        public override string TargetMod => "kentington.saveourship2";
+
+        public override IEnumerable<IModCompatibilityPatchComponent> GetComponents()
+        {
+            return new List<IModCompatibilityPatchComponent>()
+            {
+                new Patch_ShipInteriorMod2_MoveShip(),
+            };
+        }
+    }
+}
